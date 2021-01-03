ef91d8120bd870a599dc00f2e2aae765d1a21c54
JeffreyCao <jeffreycao1024@gmail.com>
2021-01-03 12:27:46 +0800

    video support

--- caphone.sh
+++ caphone.sh
@@ -1,10 +1,19 @@
 #! /bin/sh
 if [ ! -n "$1" ] ;then
-    png_file="screencap_zcoding.png"
+    out_file="screencap_zcoding.png"
 else
-    png_file=$1
+    out_file=$1
 fi
 
-adb shell /system/bin/screencap -p /sdcard/screencap_zcoding.png
-adb pull /sdcard/screencap_zcoding.png $png_file
-adb shell rm /sdcard/screencap_zcoding.png
+if [ "${out_file##*.}"x = "mp4"x ];then
+    adb shell /system/bin/screenrecord /sdcard/ovo_screenrecord.mp4
+    adb pull /sdcard/ovo_screenrecord.mp4 $out_file
+    adb shell rm /sdcard/ovo_screenrecord.mp4
+else
+    adb shell /system/bin/screencap -p /sdcard/ovo_screencap.png
+    adb pull /sdcard/ovo_screencap.png $out_file
+    adb shell rm /sdcard/ovo_screencap.png
+fi
+
+
+
